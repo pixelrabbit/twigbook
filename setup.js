8f0b684bb87388a3b2c9d61a02442e37a6d13541
pixelrabbit <dangovea@gmail.com>
2022-02-25 16:11:37 -0600

    test npx script

--- setup.js
+++ setup.js
@@ -2,17 +2,20 @@
 
 console.log("this is a setup.js")
 
-// const fs = require('fs');
-// const fse = require("fs-extra");
+const fs = require('fs');
+const fse = require("fs-extra");
 
-// const srcDir = `./components`;
-// const destDir = `./test/components`;
+const srcDir = `./components`;
+var destDir = `./components`;
+if (process.argv[2]){
+    destDir = process.argv[2];
+}
 
-// // To copy a folder or file
-// fse.copySync(srcDir, destDir, { overwrite: true }, function (err) {
-//   if (err) {
-//     console.error(err);
-//   } else {
-//     console.log("success!");
-//   }
-// });
+// To copy a folder or file
+fse.copySync(srcDir, destDir, { overwrite: true }, function (err) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("success!");
+  }
+});

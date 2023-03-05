945b3c6d850fc18b43717c6518d67e035858cd23
pixelrabbit <dangovea@gmail.com>
2023-03-04 18:32:43 -0600

    finish transition to player class

--- components/video/video.js
+++ components/video/video.js
@@ -1,230 +1,156 @@
-const rabbitPlayer = {
-  init: function (selector) {
-    document.body.querySelectorAll(selector).forEach(function (player) {
-      const video = player.querySelector(".player__video");
-      const timeCurrent = player.querySelector(".player__controls__time__current");
-      const timeDuration = player.querySelector(".player__controls__time__duration");
-      const btnPlay = player.querySelector(".play");
-      const btnStop = player.querySelector(".stop");
-      const progress = player.querySelector(".player__progress");
-      const btnFullscreen = player.querySelector(".player__fullscreen");
-      const btnPip = player.querySelector(".player__pip");
-      let playTriggered = false;
-
-      rabbitPlayer.cues(video);
-
-      // interval for tracking video progress
-      let playingInterval;
-
-      // btnPlay.addEventListener("click", function () {
-      //   if (video.paused) {
-      //     video.play();
-      //     playTriggered = true;
-      //   } else {
-      //     video.pause();
-      //     playTriggered = false;
-      //   }
-      // });
-      // btnStop.addEventListener("click", function () {
-      //   video.pause();
-      //   video.currentTime = 0;
-      //   updateTime();
-      //   playTriggered = false;
-      // });
-
-      // // set duration time when video can start playing
-      // video.addEventListener("canplay", function () {
-      //   timeDuration.innerText = new Date(Math.round(video.duration) * 1000).toISOString().slice(14, 19);
-      // });
-
-      // // update current time
-      // video.addEventListener("play", () => {
-      //   trackProgress();
-      // });
-      video.addEventListener("ended", function () {
-        trackProgress(false);
-        playTriggered = false;
-      });
-      video.addEventListener("pause", function () {
-        trackProgress(false);
-      });
-
-      // PROGRESS BAR
-      // click and drag events
-      progress.addEventListener("input", function () {
-        video.pause();
-        video.currentTime = (progress.value / 100) * video.duration;
-        updateTime();
-      });
-      // when change is "committed" to value
-      progress.addEventListener("change", function () {
-        updateTime();
-        if (playTriggered) {
-          video.play();
-        }
-      });
-
-      const trackProgress = function (track = true) {
-        if (track) {
-          playingInterval = window.setInterval(function () {
-            updateTime();
-          }, 50);
-          console.log(video.buffered)
-        } else {
-          window.clearInterval(playingInterval);
-        }
-      };
-      const updateTime = function () {
-        progress.value = (video.currentTime / video.duration) * 100;
-        timeCurrent.innerText = new Date(Math.round(video.currentTime) * 1000).toISOString().slice(14, 19);
-      }
-
-      // full screen
-      btnFullscreen.addEventListener('change', (event) => {
-        if (event.currentTarget.checked) {
-          player.requestFullscreen();
-        } else {
-          document.exitFullscreen();
-        }
-      })
-      player.addEventListener("fullscreenchange", function (event) {
-        if (document.fullscreenElement) {
-          btnFullscreen.checked = true;
-        } else {
-          btnFullscreen.checked = false;
-        }
-      });
-
-      // picture in picture
-      btnPip.addEventListener('change', (event) => {
-        if (event.currentTarget.checked) {
-          player.classList.add("pip")
-        } else {
-          player.classList.remove("pip")
-        }
-      })
-
-    });
-  },
-  play: function () { },
-  cues: function (video) {
-    const track = video.getElementsByTagName('track')[0].track;
-    console.log(track)
-    // track.activeCues[0].line = -4;
-  }
-};
-
-window.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    // rabbitPlayer.init(".player");
-    const testVideo = new RabbitVideo("video", {
-      controlsPosition: "overlay"
-    });
-  },
-  { once: true }
-);
-
-
 class RabbitVideo {
-  constructor(video, options = {}) {
-    this.video = document.querySelector(video);
-    this.options = {
-      captions: true,
-      controlsPosition: "bottom",
-      ...options
-    }
-    this.playTriggered = false;
-    this.build()
-  }
-  // Getter
-  build() {
+	constructor(video, options = {}) {
+		this.video = document.querySelector(video);
+		this.options = {
+			captions: true,
+			controlsPosition: "bottom",
+			...options
+		}
+		this.playTriggered = false;
+		this.build()
+	}
+	// Getter
+	build() {
 
-    const _this = this;
+		const _this = this;
 
-    // wrap video
-    const wrapper = document.createElement('div');
-    wrapper.classList.add("player");
-    this.video.parentNode.insertBefore(wrapper, this.video);
-    wrapper.appendChild(this.video);
-    this.wrapper = wrapper;
+		// wrap video
+		this.wrapper = document.createElement('div');
+		this.wrapper.classList.add("player");
+		this.video.parentNode.insertBefore(this.wrapper, this.video);
+		this.wrapper.appendChild(this.video);
 
-    // controls
-    const controls = `
+		// controls
+		const controls = `
     <div class="player__controls">
       <input type="range" class="player__progress" min="0" max="100" step="any" value="0" />
-      <div class="player__controls__group">
-        <button class="play">Play/Pause</button>
-        <button class="stop">Stop</button>
+
+        <button class="play" aria-label="Play/pause">
+          <svg height="20" width="20">
+            <polygon class="icon--play" points="2,0 18,10 2,20" fill="#ffffff" />
+			<g class="icon--pause" fill="#ffffff">
+				<rect x="2" y="2" width="6" height="16" />
+				<rect x="12" y="2" width="6" height="16" />
+			</g>
+          </svg>
+        </button>
         <div class="player__controls__time">
-          <span class="player__controls__time__current">00:00</span>
-          /
-          <span class="player__controls__time__duration">00:00</span>
+          <span class="player__controls__time__current">00:00</span>&nbsp;/&nbsp;<span class="player__controls__time__duration">00:00</span>
         </div>
-      </div>
-      <div class="player__controls__group">
         <button class="player__transcript">Transcript</button>
         <button class="player__captions">Captions</button>
         <label><input type="checkbox" name="fullscreen" class="player__fullscreen">Full screen</label>
         <label><input type="checkbox" name="picture-in-picture" class="player__pip">Picture in picture</label>
-      </div>
     </div>
     `;
-    _this.wrapper.insertAdjacentHTML("beforeend", controls);
+		_this.wrapper.insertAdjacentHTML("beforeend", controls);
 
-    this.timeCurrent = _this.wrapper.querySelector(".player__controls__time__current");
-    const timeDuration = _this.wrapper.querySelector(".player__controls__time__duration");
-    const btnPlay = _this.wrapper.querySelector(".play");
-    const btnStop = _this.wrapper.querySelector(".stop");
-    this.progress = _this.wrapper.querySelector(".player__progress");
-    const btnFullscreen = _this.wrapper.querySelector(".player__fullscreen");
-    const btnPip = _this.wrapper.querySelector(".player__pip");
+		this.timeCurrent = this.wrapper.querySelector(".player__controls__time__current");
+		this.timeDuration = this.wrapper.querySelector(".player__controls__time__duration");
+		this.btnPlay = this.wrapper.querySelector(".play");
+		const btnStop = this.wrapper.querySelector(".stop");
+		this.progress = this.wrapper.querySelector(".player__progress");
+		this.btnFullscreen = this.wrapper.querySelector(".player__fullscreen");
+		const btnPip = this.wrapper.querySelector(".player__pip");
 
-    // play button listener
-    btnPlay.addEventListener("click", function(){
-      if (_this.video.paused) {
-        _this.play();
-      } else {
-        _this.pause(); 
-      }
-    });
+		// play button listener
+		this.btnPlay.addEventListener("click",  () => {
+			if (this.video.paused) {
+				this.play();
+			} else {
+				this.pause();
+			}
+		});
 
-    // stop button listener
-    btnStop.addEventListener("click", function () {
-      _this.pause(); 
-      _this.video.currentTime = 0;
-      // updateTime();
-      _this.playTriggered = false;
-    });
+		// stop button listener
+		// btnStop.addEventListener("click", () => {
+		// 	this.pause();
+		// 	this.video.currentTime = 0;
+		// 	this.progress.value = 0;
+		// 	this.playTriggered = false;
+		// });
 
-    // set duration time when video can start playing
-    this.video.addEventListener("canplay", () => {
-      timeDuration.innerText = new Date(Math.round(_this.video.duration) * 1000).toISOString().slice(14, 19);
-    });
+		// set duration time when video can start playing
+		this.video.addEventListener("canplay", () => {
+			this.timeDuration.innerText = new Date(Math.round(_this.video.duration) * 1000).toISOString().slice(14, 19);
+		});
 
-    // update current time
-    this.video.addEventListener("play", () => {
-      _this.trackProgress();
-    });
-  }
+		// video play event
+		this.video.addEventListener("play", () => {
+			// custom interval so progress bar is updated more frequently
+			this.playingInterval = window.setInterval(() => {
+				// console.log("playingInterval")
+				this.progress.value = (this.video.currentTime / this.video.duration) * 100;
+			}, 50);
+			this.wrapper.classList.add("playing");
+		});
 
-  play(){
-     this.video.play();
-     this.playTriggered = true;
-  }
-  pause(){
-    this.video.pause();
-    this.playTriggered = false;
-  }
-  trackProgress(){
-    // if (track) {
-      _this = this;
-      playingInterval = window.setInterval(function () {
-        _this.progress.value = (_this.video.currentTime / _this.video.duration) * 100;
-        _this.timeCurrent.innerText = new Date(Math.round(_this.video.currentTime) * 1000).toISOString().slice(14, 19);
-      }, 50);
-    // } else {
-    //   window.clearInterval(playingInterval);
-    // }
-  }
+		// video pause event
+		this.video.addEventListener("pause", () => {
+			// remove custom interval
+			window.clearInterval(this.playingInterval);
+			this.wrapper.classList.remove("playing");
+		});
+
+		// update time using time update, more efficient than interval
+		this.video.addEventListener("timeupdate", (e) => {
+			// console.log(e)
+			_this.timeCurrent.innerText = new Date(Math.round(_this.video.currentTime) * 1000).toISOString().slice(14, 19);
+		});
+
+		// interaction with progress bar
+		this.progress.addEventListener("input", () => {
+			this.video.pause();
+			this.video.currentTime = (this.progress.value / 100) * this.video.duration;
+
+		});
+		// when progress change is "committed" to value
+		this.progress.addEventListener("change", () => {
+			if (this.playTriggered) {
+				this.video.play();
+			}
+		});
+		// if video ends, remove playTrigger set to false
+		this.video.addEventListener("ended", () => {
+			this.playTriggered = false;
+		});
+
+		// full screen controls
+		this.btnFullscreen.addEventListener('change', (event) => {
+			if (event.currentTarget.checked) {
+				this.wrapper.requestFullscreen();
+			} else {
+				document.exitFullscreen();
+			}
+		})
+		this.wrapper.addEventListener("fullscreenchange", (event) => {
+			if (document.fullscreenElement) {
+				this.btnFullscreen.checked = true;
+			} else {
+				this.btnFullscreen.checked = false;
+			}
+		});
+	}
+
+	play() {
+		this.video.play();
+		this.playTriggered = true;
+	}
+
+	pause() {
+		this.video.pause();
+		this.playTriggered = false;
+	}
 }
+
+
+window.addEventListener(
+	"DOMContentLoaded",
+	function () {
+		// rabbitPlayer.init(".player");
+		const testVideo = new RabbitVideo("video", {
+			controlsPosition: "overlay"
+		});
+	},
+	{ once: true }
+);

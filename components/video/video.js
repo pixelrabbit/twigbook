62f1eac4b0c5286fb3793a34e9e8fcdb9edce8aa
pixelrabbit <dangovea@gmail.com>
2023-03-10 17:28:38 -0600

    research chapters

--- components/video/video.js
+++ components/video/video.js
@@ -7,6 +7,7 @@ class RabbitVideo {
 			...options
 		}
 		this.playTriggered = false;
+		this.chaptersObj = {};
 		this.build()
 	}
 	// Getter
@@ -21,8 +22,9 @@ class RabbitVideo {
 		// controls
 		const controls = `
     <div class="player__controls">
-	
-      <input type="range" class="player__progress" min="0" max="100" step="any" value="0" />
+		<div class="player__progress">
+      <input type="range" min="0" max="100" step="any" value="0" />
+	  </div>
         <button class="player__play" aria-label="Play/pause">
 		  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
 		 		 <path class="icon--play" d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
@@ -35,12 +37,12 @@ class RabbitVideo {
         </div>
 		<button class="player__mute">
 		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-			<g class="icon--volume">
+			<g class="icon--volume-high">
 			<path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
 			<path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
-			<path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"/>
-		  
+			<path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"/>	  
 			</g>
+			<path class="icon--volume-low" d="M9 4a.5.5 0 0 0-.812-.39L5.825 5.5H3.5A.5.5 0 0 0 3 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 9 12V4zm3.025 4a4.486 4.486 0 0 1-1.318 3.182L10 10.475A3.489 3.489 0 0 0 11.025 8 3.49 3.49 0 0 0 10 5.525l.707-.707A4.486 4.486 0 0 1 12.025 8z"/>
 			<path class="icon--mute" d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/>
 	  </svg>
 			</button>
@@ -48,7 +50,12 @@ class RabbitVideo {
 			
 			<input class="player__volume__range" type="range" min="0" max="1" step="0.1" value="1" /> 
 		</div>
-        <button class="player__transcript">Transcript</button>
+        <button class="player__transcript">
+		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+		<path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+		<path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"/>
+	  </svg>
+		</button>
         <button class="player__captions">
 		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
 		<path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm3.027 4.002c-.83 0-1.319.642-1.319 1.753v.743c0 1.107.48 1.727 1.319 1.727.69 0 1.138-.435 1.186-1.05H7.36v.114c-.057 1.147-1.028 1.938-2.342 1.938-1.613 0-2.518-1.028-2.518-2.729v-.747C2.5 6.051 3.414 5 5.018 5c1.318 0 2.29.813 2.342 2v.11H6.213c-.048-.638-.505-1.108-1.186-1.108zm6.14 0c-.831 0-1.319.642-1.319 1.753v.743c0 1.107.48 1.727 1.318 1.727.69 0 1.139-.435 1.187-1.05H13.5v.114c-.057 1.147-1.028 1.938-2.342 1.938-1.613 0-2.518-1.028-2.518-2.729v-.747c0-1.7.914-2.751 2.518-2.751 1.318 0 2.29.813 2.342 2v.11h-1.147c-.048-.638-.505-1.108-1.187-1.108z"/>
@@ -68,12 +75,12 @@ class RabbitVideo {
 		this.timeCurrent = this.player.querySelector(".player__controls__time__current");
 		this.timeDuration = this.player.querySelector(".player__controls__time__duration");
 		this.btnPlay = this.player.querySelector(".player__play");
-		// const btnStop = this.player.querySelector(".stop");
-		this.progress = this.player.querySelector(".player__progress");
+		this.progress = this.player.querySelector(".player__progress > input");
 		this.btnFullscreen = this.player.querySelector(".player__fullscreen");
 		// const btnPip = this.player.querySelector(".player__pip");
 		this.volume = this.player.querySelector(".player__volume__range");
 		this.mute = this.player.querySelector(".player__mute");
+		this.chapters = this.player.querySelector("code");
 
 		this.video.addEventListener('click', () => {
 			if (this.video.paused) {
@@ -92,17 +99,10 @@ class RabbitVideo {
 			}
 		});
 
-		// stop button listener
-		// btnStop.addEventListener("click", () => {
-		// 	this.pause();
-		// 	this.video.currentTime = 0;
-		// 	this.progress.value = 0;
-		// 	this.playTriggered = false;
-		// });
-
 		// set duration time when video can start playing
 		this.video.addEventListener("canplay", () => {
 			this.timeDuration.innerText = new Date(Math.round(this.video.duration) * 1000).toISOString().slice(14, 19);
+			setVolumeIcon();
 		});
 
 		// // waiting or buffering
@@ -119,7 +119,6 @@ class RabbitVideo {
 
 		// video play event
 		this.video.addEventListener("play", () => {
-			console.log("play");
 			// custom interval so progress bar is updated more frequently
 			this.playingInterval = window.setInterval(() => {
 				// console.log("playingInterval")
@@ -167,15 +166,31 @@ class RabbitVideo {
 		// toggle mute on button press
 		this.mute.addEventListener("click",()=>{
 			if(this.video.volume > 0){
-				this.mute.classList.add("muted");
+				this.currentVolume = this.video.volume;
 				this.volume.value = 0;
 				this.video.volume = 0;
 			} else {
-				this.mute.classList.remove("muted");
-				this.volume.value = 0.7;
-				this.video.volume = 0.7;
+				this.volume.value = this.currentVolume;
+				this.video.volume = this.currentVolume;
 			}
 		})
+		// volume change, change button icon
+		this.video.addEventListener("volumechange", () => {
+			setVolumeIcon();
+		})
+		const setVolumeIcon = () => {
+			var vol = this.video.volume;
+			if(vol == 0){
+				this.mute.classList.add("muted");
+				this.mute.classList.remove("low","high")
+			} else if (vol <= 0.5){
+				this.mute.classList.add("low");
+				this.mute.classList.remove("muted","high")
+			} else {
+				this.mute.classList.add("high");
+				this.mute.classList.remove("muted","low")
+			}
+		}
 
 		// full screen controls
 		this.btnFullscreen.addEventListener('click', (event) => {
@@ -192,8 +207,12 @@ class RabbitVideo {
 				this.btnFullscreen.value = "false";
 			}
 		});
-	}
 
+		// build chapters
+		let chaptersObj = this.chapters.innerText;
+
+	
+	}
 	play() {
 		this.video.play();
 		this.playTriggered = true;

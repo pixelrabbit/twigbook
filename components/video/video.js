d052305bd9b454fd5a9912a3f97eab68b9b5114e
pixelrabbit <dangovea@gmail.com>
2023-03-09 22:51:38 -0600

    switch to bootstrap icons

--- components/video/video.js
+++ components/video/video.js
@@ -23,48 +23,43 @@ class RabbitVideo {
     <div class="player__controls">
 	
       <input type="range" class="player__progress" min="0" max="100" step="any" value="0" />
-
         <button class="player__play" aria-label="Play/pause">
-          <svg height="20" width="20">
-            <polygon class="icon--play" points="2,0 18,10 2,20" fill="#ffffff" />
-			<g class="icon--pause" fill="#ffffff">
-				<rect x="2" y="2" width="6" height="16" />
-				<rect x="12" y="2" width="6" height="16" />
-			</g>
-			<g class="icon--buffering" fill="#ffffff">
-				<circle cx="4"  cy="10" r="2" />
-				<circle cx="10"  cy="10" r="2" />
-				<circle cx="16"  cy="10" r="2" />
-			</g>
-          </svg>
+		  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+		 		 <path class="icon--play" d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+				  <path class="icon--pause" d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z"/>
+				  <path class="icon--buffering" d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+			</svg>     
         </button>
         <div class="player__controls__time">
           <span class="player__controls__time__current">00:00</span>&nbsp;/&nbsp;<span class="player__controls__time__duration">00:00</span>
         </div>
+		<button class="player__mute">
+		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+			<g class="icon--volume">
+			<path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
+			<path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
+			<path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"/>
+		  
+			</g>
+			<path class="icon--mute" d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/>
+	  </svg>
+			</button>
 		<div class="player__volume">
+			
 			<input class="player__volume__range" type="range" min="0" max="1" step="0.1" value="1" /> 
 		</div>
         <button class="player__transcript">Transcript</button>
         <button class="player__captions">
-			<svg height="20" width="20">
-				<rect x="1" y="3" width="18" height="14" fill="transparent" stroke-width="2" stroke="#ffffff"/>
-			</svg>
+		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+		<path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm3.027 4.002c-.83 0-1.319.642-1.319 1.753v.743c0 1.107.48 1.727 1.319 1.727.69 0 1.138-.435 1.186-1.05H7.36v.114c-.057 1.147-1.028 1.938-2.342 1.938-1.613 0-2.518-1.028-2.518-2.729v-.747C2.5 6.051 3.414 5 5.018 5c1.318 0 2.29.813 2.342 2v.11H6.213c-.048-.638-.505-1.108-1.186-1.108zm6.14 0c-.831 0-1.319.642-1.319 1.753v.743c0 1.107.48 1.727 1.318 1.727.69 0 1.139-.435 1.187-1.05H13.5v.114c-.057 1.147-1.028 1.938-2.342 1.938-1.613 0-2.518-1.028-2.518-2.729v-.747c0-1.7.914-2.751 2.518-2.751 1.318 0 2.29.813 2.342 2v.11h-1.147c-.048-.638-.505-1.108-1.187-1.108z"/>
+	  </svg>
 		</button>
         <button class="player__fullscreen">
-			<svg height="20" width="20">
-				<g class="icon--expand" fill="#ffffff">
-					<polygon points="1,1 7,1 5,3 8,6 6,8 3,5 1,7" />
-					<polygon points="20,1 20,7 18,5 15,8 13,6 16,3 14,1" />
-					<polygon points="20,20 14,20 16,18 13,15 15,13 18,16 20,14" />
-					<polygon points="1,20 1,14 3,16 6,13 8,15 5,18 7,20" />
-				</g>
-				<g class="icon--collapse" fill="#ffffff">
-					<polygon points="3,1 6,4 8,2 8,8 2,8 4,6 1,3" />
-					<polygon points="13,8 13,2 15,4 18,1 20,3 17,6 19,8" />
-					<polygon points="13,13 19,13 17,15 20,18 18,20 15,17 13,19" />
-					<polygon points="8,13 8,19 6,17 3,20 1,18 4,15 2,13" />
-				</g>
-			</svg>
+		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+			<path class="icon--expand" d="M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z"/>
+			<path class="icon--collapse" d="M5.5 0a.5.5 0 0 1 .5.5v4A1.5 1.5 0 0 1 4.5 6h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5zm5 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 10 4.5v-4a.5.5 0 0 1 .5-.5zM0 10.5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 6 11.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zm10 1a1.5 1.5 0 0 1 1.5-1.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4z"/>
+
+	  </svg>
 		</button>
     </div>
     `;
@@ -78,8 +73,9 @@ class RabbitVideo {
 		this.btnFullscreen = this.player.querySelector(".player__fullscreen");
 		// const btnPip = this.player.querySelector(".player__pip");
 		this.volume = this.player.querySelector(".player__volume__range");
+		this.mute = this.player.querySelector(".player__mute");
 
-		this.video.addEventListener('click',()=>{
+		this.video.addEventListener('click', () => {
 			if (this.video.paused) {
 				this.play();
 			} else {
@@ -166,6 +162,19 @@ class RabbitVideo {
 		// adjust volume on volume range change
 		this.volume.addEventListener("change", (e) => {
 			this.video.volume = e.target.value;
+		})
+
+		// toggle mute on button press
+		this.mute.addEventListener("click",()=>{
+			if(this.video.volume > 0){
+				this.mute.classList.add("muted");
+				this.volume.value = 0;
+				this.video.volume = 0;
+			} else {
+				this.mute.classList.remove("muted");
+				this.volume.value = 0.7;
+				this.video.volume = 0.7;
+			}
 		})
 
 		// full screen controls
